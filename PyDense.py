6e532ce5be71969de5a4eee1efa260177362d794
Jigyass <jigyas@icloud.com>
2024-04-23 22:36:54 -0500

    Python Script for training

---
+++ PyDense.py
@@ -0,0 +1,106 @@
+import tensorflow as tf
+import matplotlib.pyplot as plt
+from tensorflow.keras.preprocessing import image_dataset_from_directory
+from tensorflow.keras.applications import DenseNet121
+from tensorflow.keras.layers import Input
+from tensorflow.keras.models import Model
+
+def preprocess_image(image, label):
+    image = tf.cast(image, tf.float32) / 255.0
+    image = tf.image.resize(image, [224, 224], method='bilinear')
+    image = tf.image.grayscale_to_rgb(image)
+    return image, label
+
+def prepare_dataset(directory, batch_size=32):
+    dataset = image_dataset_from_directory(
+        directory,
+        labels='inferred',
+        label_mode='int',
+        image_size=(48, 48),
+        color_mode='grayscale',
+        batch_size=batch_size,
+        shuffle=True
+    )
+    dataset = dataset.map(preprocess_image, num_parallel_calls=tf.data.AUTOTUNE)
+    dataset = dataset.prefetch(buffer_size=tf.data.AUTOTUNE)
+    return dataset
+
+def show_batch(image_batch, label_batch):
+    plt.figure(figsize=(10,10))
+    for n in range(min(len(image_batch), 25)):
+        ax = plt.subplot(5,5,n+1)
+        plt.imshow(image_batch[n])
+        plt.title(label_batch[n].numpy())
+        plt.axis('off')
+    plt.show()
+
+def prepare_datasets(directory, batch_size=100, train_val_split=0.8):
+    total_files = len(tf.io.gfile.glob(directory + '/*/*.jpg'))
+    train_size = int(train_val_split * total_files)
+    val_size = total_files - train_size
+    
+    dataset = image_dataset_from_directory(
+        directory,
+        validation_split=1-train_val_split,
+        subset="training",
+        seed=123,
+        labels='inferred',
+        label_mode='categorical',
+        image_size=(48, 48),
+        color_mode='grayscale',
+        batch_size=batch_size,
+        shuffle=True
+    )
+
+    validation_dataset = image_dataset_from_directory(
+        directory,
+        validation_split=1-train_val_split,
+        subset="validation",
+        seed=123,
+        labels='inferred',
+        label_mode='categorical',
+        image_size=(48, 48),
+        color_mode='grayscale',
+        batch_size=batch_size,
+        shuffle=True
+    )
+
+    dataset = dataset.map(preprocess_image, num_parallel_calls=tf.data.AUTOTUNE)
+    validation_dataset = validation_dataset.map(preprocess_image, num_parallel_calls=tf.data.AUTOTUNE)
+
+    dataset = dataset.prefetch(buffer_size=tf.data.AUTOTUNE)
+    validation_dataset = validation_dataset.prefetch(buffer_size=tf.data.AUTOTUNE)
+    return dataset, validation_dataset
+
+def build_model(num_classes):
+    input_tensor = Input(shape=(224, 224, 3))
+    base_model = DenseNet121(include_top=False, weights='imagenet', input_tensor=input_tensor)
+    x = tf.keras.layers.GlobalAveragePooling2D()(base_model.output)
+    x = tf.keras.layers.Dense(num_classes, activation='softmax')(x)
+    model = Model(inputs=base_model.input, outputs=x)
+    return model
+
+if __name__ == "__main__":
+    train_directory = '/home/jigyas/Github/FacialExpressionRecognitionUsingDensenet121/project_1_dataset/train'
+    train_dataset = prepare_dataset(train_directory)
+
+    for images, labels in train_dataset.take(1):
+        show_batch(images, labels)
+
+    model = build_model(7)
+    model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+    train_dataset, validation_dataset = prepare_datasets(train_directory)
+    
+    history = model.fit(
+        train_dataset,
+        validation_data=validation_dataset,
+        epochs=10
+    )
+
+    DenseCNN = '/home/jigyas/Github/FacialExpressionRecognitionUsingDensenet121/NewDenseCNN'
+    Densehd = '/home/jigyas/Github/FacialExpressionRecognitionUsingDensenet121/NewDense.h5'
+    DenseKeras = '/home/jigyas/Github/FacialExpressionRecognitionUsingDensenet121/NewDenseKeras.keras'
+    model.save(DenseCNN)
+    # model.save(Densehd, save_format='h5')
+    model.save(DenseKeras, save_format='keras')
+
